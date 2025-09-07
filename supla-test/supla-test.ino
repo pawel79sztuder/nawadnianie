49aa0f003b96d5c381df830d3885d34a0a20d2f9
pawel79sztuder <pawelsz223@gmail.com>
2025-09-07 14:46:18 +0200

    Update supla-test.ino

--- supla-test/supla-test.ino
+++ supla-test/supla-test.ino
@@ -1,21 +1,20 @@
 #include <WiFi.h>
-#include <supla/network/esp_wifi.h>
 #include <supla/esp32.h>
 
-// Dane WiFi
 #define WIFI_SSID     "Pawel_LTE"
 #define WIFI_PASSWORD "pawel2580s"
 
-// Dane Supla (Twoje GUID i hasło do urządzenia SUPLA)
-#define SUPLA_GUID    "2593"       // GUID urządzenia SUPLA
-#define SUPLA_AUTHKEY "d39f0361"    // Hasło/authkey do urządzenia SUPLA
+#define SUPLA_GUID    "2593"
+#define SUPLA_AUTHKEY "d39f0361"
 
-// Pin przekaźnika
 #define RELAY_PIN 2
 
-// Utworzenie obiektu WiFi i urządzenia Supla
 Supla::ESPWifi wifi(WIFI_SSID, WIFI_PASSWORD);
 Supla::ESP32 device(wifi, SUPLA_GUID, SUPLA_AUTHKEY);
+
+void onRelayChanged(bool state) {
+  digitalWrite(RELAY_PIN, state ? HIGH : LOW);
+}
 
 void setup() {
   Serial.begin(115200);
@@ -24,13 +23,13 @@ void setup() {
   pinMode(RELAY_PIN, OUTPUT);
   digitalWrite(RELAY_PIN, LOW);
 
+  // Dodanie kanału przekaźnika z callbackiem
+  device.addRelay(1, onRelayChanged);
+
   device.begin();
   Serial.println("SuplaDevice started");
 }
 
 void loop() {
   device.loop();
-
-  // Przykładowa obsługa przekaźnika - sterowanie przez SUPLA kanał (jeśli dodane)
-  // Można rozszerzyć o obsługę kanałów i zdarzeń, zależnie od potrzeb
 }
