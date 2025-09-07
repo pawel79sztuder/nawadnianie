f5802e23d49e0a632b1446bc8ad995edb8b3793b
pawel79sztuder <pawelsz223@gmail.com>
2025-09-07 14:55:38 +0200

    Update supla-test.ino

--- supla-test/supla-test.ino
+++ supla-test/supla-test.ino
@@ -1,16 +1,18 @@
 #include <WiFi.h>
 #include <SuplaDevice.h>
+#include <SuplaDeviceGui.h>
 
+// Twoje dane WiFi
 #define WIFI_SSID     "Pawel_LTE"
 #define WIFI_PASSWORD "pawel2580s"
 
-#define SUPLA_GUID    "2593"
-#define SUPLA_AUTHKEY "d39f0361"
+// Dane SUPLA - pamiętaj, że GUID musi mieć 16 znaków, a AUTHKEY 8 znaków
+#define SUPLA_GUID    "000000000002593"  // tu przykładowo dodaj brakujące zera, 16 znaków
+#define SUPLA_AUTHKEY "d39f0361"         // 8 znaków
 
 #define RELAY_PIN 2
 
-// Tworzymy kanał przekaźnika (id kanału = 1, steruje pinem RELAY_PIN)
-SuplaSwitch relay(1, RELAY_PIN);
+SuplaDeviceGui gui;
 
 void setup() {
   Serial.begin(115200);
@@ -19,16 +21,34 @@ void setup() {
   pinMode(RELAY_PIN, OUTPUT);
   digitalWrite(RELAY_PIN, LOW);
 
-  // Inicjalizacja SuplaDevice z GUID i authkey
-  SuplaDevice.begin(SUPLA_GUID, SUPLA_AUTHKEY);
+  // Połączenie WiFi - możesz dodać timeout itp.
+  WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
+  Serial.print("Connecting to WiFi");
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println(" connected");
 
-  // Dodaj kanał przekaźnika
-  SuplaDevice.addChannel(&relay);
+  // Uruchomienie SuplaDevice z wymaganymi 5 argumentami
+  bool started = SuplaDevice.begin(
+    SUPLA_GUID,
+    SUPLA_AUTHKEY,
+    WIFI_SSID,
+    WIFI_PASSWORD,
+    23 // wersja protokołu SUPLA (domyślna 23)
+  );
 
-  Serial.println("SuplaDevice started");
+  if (!started) {
+    Serial.println("Failed to start SuplaDevice");
+  } else {
+    Serial.println("SuplaDevice started");
+  }
+
+  // Dodaj przełącznik (relay) pod pinem RELAY_PIN do GUI Supla (automatycznie wyśle stan)
+  gui.addSwitch(RELAY_PIN);
 }
 
 void loop() {
-  // Obsługa komunikacji z SUPLA
   SuplaDevice.loop();
 }
