438483c8df97b251138cdcdb2565da32249398a2
pawel79sztuder <pawelsz223@gmail.com>
2025-09-07 14:41:41 +0200

    Update supla-test.ino

--- supla-test/supla-test.ino
+++ supla-test/supla-test.ino
@@ -1,21 +1,21 @@
 #include <WiFi.h>
-#include <SuplaDevice.h>
-
+#include <supla/network/esp_wifi.h>
+#include <supla/esp32.h>
 
 // Dane WiFi
 #define WIFI_SSID     "Pawel_LTE"
 #define WIFI_PASSWORD "pawel2580s"
 
-// Dane Supla
-#define SUPLA_SERVER  "my.supla.org"
-#define SUPLA_PORT    2015
-#define SUPLA_LOGIN   "psztuder223@interia.pl"
-#define SUPLA_PASS    "pawel2580s"
+// Dane Supla (Twoje GUID i hasło do urządzenia SUPLA)
+#define SUPLA_GUID    "2593"       // GUID urządzenia SUPLA
+#define SUPLA_AUTHKEY "d39f0361"    // Hasło/authkey do urządzenia SUPLA
 
-// Przekaźnik podłączony do GPIO 2
+// Pin przekaźnika
 #define RELAY_PIN 2
 
-SuplaSwitch relay(1, RELAY_PIN);
+// Utworzenie obiektu WiFi i urządzenia Supla
+Supla::ESPWifi wifi(WIFI_SSID, WIFI_PASSWORD);
+Supla::ESP32 device(wifi, SUPLA_GUID, SUPLA_AUTHKEY);
 
 void setup() {
   Serial.begin(115200);
@@ -24,26 +24,13 @@ void setup() {
   pinMode(RELAY_PIN, OUTPUT);
   digitalWrite(RELAY_PIN, LOW);
 
-  Serial.println("Łączenie z WiFi...");
-  WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
-
-  while (WiFi.status() != WL_CONNECTED) {
-    delay(500);
-    Serial.print(".");
-  }
-  Serial.println("\nWiFi połączone");
-
-  // Inicjalizacja Supla
-  SuplaDevice.begin(1);  // ID urządzenia Supla = 1
-  SuplaDevice.setServer(SUPLA_SERVER, SUPLA_PORT);
-  SuplaDevice.setLogin(SUPLA_LOGIN);
-  SuplaDevice.setPassword(SUPLA_PASS);
-
-  SuplaDevice.addChannel(&relay);
-
-  Serial.println("Supla started");
+  device.begin();
+  Serial.println("SuplaDevice started");
 }
 
 void loop() {
-  SuplaDevice.loop();
+  device.loop();
+
+  // Przykładowa obsługa przekaźnika - sterowanie przez SUPLA kanał (jeśli dodane)
+  // Można rozszerzyć o obsługę kanałów i zdarzeń, zależnie od potrzeb
 }
