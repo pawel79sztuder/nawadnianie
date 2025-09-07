61a95549f8f492ab9762387336d462641d43c8a4
pawel79sztuder <pawelsz223@gmail.com>
2025-09-07 15:37:11 +0200

    Update supla-test.ino

--- supla-test/supla-test.ino
+++ supla-test/supla-test.ino
@@ -1,6 +1,5 @@
 #include <WiFi.h>
 #include <SuplaDevice.h>
-#include <SuplaDeviceChannel.h>  // kanały
 
 #define WIFI_SSID     "Pawel_LTE"
 #define WIFI_PASSWORD "pawel2580s"
@@ -9,9 +8,6 @@
 #define SUPLA_AUTHKEY "d39f0361"
 
 #define RELAY_PIN 2
-
-// Tworzymy kanał przekaźnika
-SuplaDeviceChannel relayChannel;
 
 void setup() {
   Serial.begin(115200);
@@ -27,16 +23,14 @@ void setup() {
   }
   Serial.println("\nWiFi connected!");
 
-  // Inicjalizujemy urządzenie Supla (protokół 23 jest standardem)
+  // Inicjalizacja SuplaDevice - tylko GUID i AUTHKEY
   SuplaDevice.begin(SUPLA_GUID, SUPLA_AUTHKEY);
 
-  // Konfigurujemy kanał przekaźnika
-  relayChannel.setType(SUPLA_CHANNELTYPE_RELAY);       // typ kanału relay
-  relayChannel.setPin(RELAY_PIN);                       // podpinamy pin
-  SuplaDevice.addChannel(&relayChannel);                // dodajemy kanał do urządzenia
+  // Dodaj kanał przekaźnika (typ relay, pin RELAY_PIN)
+  SuplaDevice.addChannel(RELAY_PIN, SUPLA_CHANNELTYPE_RELAY);
 
-  // Uruchamiamy SUPLA
   SuplaDevice.setup();
+
   Serial.println("SuplaDevice setup completed");
 }
 
