7c74ad7f54fd38af152771904726e3dfb1b5d43e
pawel79sztuder <pawelsz223@gmail.com>
2025-09-07 15:04:51 +0200

    Update supla-test.ino

--- supla-test/supla-test.ino
+++ supla-test/supla-test.ino
@@ -1,16 +1,11 @@
 #include <WiFi.h>
 #include <SuplaDevice.h>
-
-#define WIFI_SSID     "Pawel_LTE"
-#define WIFI_PASSWORD "pawel2580s"
-
-// GUID musi mieć dokładnie 16 znaków (wypełnij zerami z przodu)
-#define SUPLA_GUID    "0000000000002593"
-#define SUPLA_AUTHKEY "d39f0361"
+#include <SuplaConfigESP.h>  // Konfiguracja WiFi dla ESP32
 
 #define RELAY_PIN 2
 
-SuplaSwitch relay(1, RELAY_PIN);
+// Tworzymy kanał przekaźnika o ID 1, podłączony do pinu RELAY_PIN
+SuplaRelay relay(1, RELAY_PIN);
 
 void setup() {
   Serial.begin(115200);
@@ -19,31 +14,30 @@ void setup() {
   pinMode(RELAY_PIN, OUTPUT);
   digitalWrite(RELAY_PIN, LOW);
 
-  WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
-  Serial.print("Connecting to WiFi");
-  while (WiFi.status() != WL_CONNECTED) {
-    delay(500);
-    Serial.print(".");
-  }
-  Serial.println(" connected");
+  // Ustawienie danych WiFi i SUPLA - można ustawić ręcznie lub przez webconfig
+  // Jeśli chcesz ustawić ręcznie, odkomentuj poniższy blok (jednorazowo),
+  // potem wykomentuj i zaprogramuj ponownie, żeby nie nadpisywać configu.
+  /*
+  SuplaConfig.setSSID("Pawel_LTE");
+  SuplaConfig.setPassword("pawel2580s");
+  SuplaConfig.setGUID("0000000000002593");    // 12 znaków HEX
+  SuplaConfig.setAuthKey("d39f0361");         // 8 znaków HEX
+  SuplaConfig.save();
+  */
 
-  bool started = SuplaDevice.begin(
-    SUPLA_GUID,
-    SUPLA_AUTHKEY,
-    WIFI_SSID,
-    WIFI_PASSWORD,
-    23  // wersja protokołu SUPLA
-  );
-
-  if (!started) {
-    Serial.println("Failed to start SuplaDevice");
+  // Inicjalizacja SuplaDevice z domyślną wersją protokołu (23)
+  if (!SuplaDevice.begin()) {
+    Serial.println("SuplaDevice.begin() failed");
+    while(1) { delay(1000); }
   } else {
     Serial.println("SuplaDevice started");
   }
 
+  // Dodanie kanału przekaźnika do urządzenia Supla
   SuplaDevice.addChannel(&relay);
 }
 
 void loop() {
+  // Obsługa komunikacji z SUPLA, musi być wywoływane w loop()
   SuplaDevice.loop();
 }
