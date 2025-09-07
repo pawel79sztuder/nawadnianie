a5d9c207664b4ce2df00d7b2b09180b5fba4fb2a
pawel79sztuder <pawelsz223@gmail.com>
2025-09-07 15:12:00 +0200

    Update supla-test.ino

--- supla-test/supla-test.ino
+++ supla-test/supla-test.ino
@@ -1,18 +1,16 @@
 #include <WiFi.h>
 #include <SuplaDevice.h>
-#include <SuplaDeviceESP32.h>   // konieczne dla ESP32
 #include <SuplaConfigESP.h>     // konfiguracja WiFi i Supla
 
 #define WIFI_SSID     "Pawel_LTE"
 #define WIFI_PASSWORD "pawel2580s"
 
-// GUID musi mieć dokładnie 12 bajtów (24 znaki hex), authkey 8 bajtów (16 znaków hex)
-const char SUPLA_GUID[]    = "0000000000002593";  // 12 bajtów w hex (tutaj 16 znaków, dopasuj dokładnie do wymagań Supla!)
-const char SUPLA_AUTHKEY[] = "d39f0361";          // 8 bajtów w hex (tutaj 8 znaków, dopasuj!)
+// GUID musi mieć 12 bajtów (24 znaki hex), AuthKey 8 bajtów (16 znaków hex)
+const char SUPLA_GUID[]    = "000000000000259300000000";  // dopasuj do 24 znaków!
+const char SUPLA_AUTHKEY[] = "d39f0361d39f0361";          // dopasuj do 16 znaków!
 
 #define RELAY_PIN 2
 
-// Utworzenie obiektu przekaźnika (kanał)
 SuplaSwitch relay(1, RELAY_PIN);
 
 void setup() {
@@ -21,15 +19,12 @@ void setup() {
   pinMode(RELAY_PIN, OUTPUT);
   digitalWrite(RELAY_PIN, LOW);
 
-  // Konfiguracja Supla - jeśli nie chcesz ustawiać ręcznie przez webconfig,
-  // możesz zakomentować poniższy blok, jeśli konfiguracja jest już zapisana.
   SuplaConfig.setSSID(WIFI_SSID);
   SuplaConfig.setPassword(WIFI_PASSWORD);
   SuplaConfig.setGUID(SUPLA_GUID);
   SuplaConfig.setAuthKey(SUPLA_AUTHKEY);
   SuplaConfig.save();
 
-  // Uruchomienie SuplaDevice
   if (!SuplaDevice.begin()) {
     Serial.println("SuplaDevice.begin() failed");
     while (1) delay(1000);
@@ -37,7 +32,6 @@ void setup() {
     Serial.println("SuplaDevice started");
   }
 
-  // Dodanie kanału przekaźnika
   SuplaDevice.addChannel(&relay);
 }
 
